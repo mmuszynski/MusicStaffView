189624bc342496e59066e2f90c3d3454713a27ae
Mike Muszynski <mmuszynski@gmail.com>
2026-01-07 13:04:13 -0500

    made debug public

--- Sources/MusicStaffView/SwiftUI/MusicStaffViewEnvironment/EnvironmentDebug.swift
+++ Sources/MusicStaffView/SwiftUI/MusicStaffViewEnvironment/EnvironmentDebug.swift
@@ -8,31 +8,12 @@
 import Foundation
 import SwiftUI
 
-//struct DebugEnviornmentKey: EnvironmentKey {
-//    static var defaultValue: Bool = false
-//}
-//
-//extension EnvironmentValues {
-//    var debug: Bool {
-//        get { self[DebugEnviornmentKey.self] }
-//        set { self[DebugEnviornmentKey.self] = newValue }
-//    }
-//}
-//
-//struct DebugModifier: ViewModifier {
-//    var debug: Bool
-//    func body(content: Content) -> some View {
-//        content
-//            .environment(\.debug, debug)
-//    }
-//}
-//
-//extension View {
-//    func debug(_ debug: Bool) -> some View {
-//        modifier(DebugModifier(debug: debug))
-//    }
-//}
-
 extension EnvironmentValues {
-    @Entry var debug: Bool = false
+    @Entry public var debug: Bool = false
+}
+
+extension View {
+    func debug(_ debug: Bool = true) -> some View {
+        environment(\.debug, debug)
+    }
 }
